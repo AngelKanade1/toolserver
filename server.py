7c07a2579d50453669adb7dd474a279f2397adb9
kanade <topjamesduan@gmail.com>
2023-06-30 14:53:43 +0800

    添加了容错功能.

--- server.py
+++ server.py
@@ -18,15 +18,25 @@ def index():
 @app.route('/upgradecost', methods=['GET', 'POST'])
 def upgradeCost():
     back_data = {}
+    err = 0
     if len(request.get_data()) != 0:
         rare = request.values.get("rare")
         techtype = request.values.get("techtype")
         before = int(request.values.get("before"))
         after = int(request.values.get("after"))
-        nowfragment = int(request.values.get("nowfragment"))
+        nowfragment = request.values.get("nowfragment")
+        if nowfragment != "":
+            nowfragment = int(nowfragment)
+        else:
+            back_data["data"] = "请填写所有数据"
+            err = 1
 
-        back_data["data"] = func.calcUpgradeCost(rare, techtype, before, after, nowfragment)
-        return json.dumps(back_data, ensure_ascii=False)
+        if before >= after:
+            back_data["data"] = "目标不能比当前数据还小"
+            err = 1
+
+        if not err:
+            back_data["data"] = func.calcUpgradeCost(rare, techtype, before, after, nowfragment)
 
     return json.dumps(back_data, ensure_ascii=False)
 
